0e9def63f7359f1370b6712f7ae29199bcc13a73
Tea067 <tawit_m@cmu.ac.th>
2025-12-09 16:11:24 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int num, n = 0, o = 0;
+    
+    while ( true ){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if ( num == 0){
+            break;
+        } else {
+            if ( num%2 == 0 ){
+            n = n+1;
+        } else {
+            o = o+1;
+        }
+        }
+    }
+    
+    cout << "#Even numbers = " << n << endl;
+    cout << "#Odd numbers = " << o << endl;
+
     return 0;
 }
